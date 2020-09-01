c3ce51084b0e89265249305b4516263489207458
Tata Adi Nugroho <tataa2237@gmail.com>
2020-09-02 05:16:28 +0700

    initial file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Autofill Form</title>
+    </head>
+    <body>
+        <center>
+            <table>
+                <tr>
+                    <td>NIM</td>
+                    <td><input type="text"></td>
+                </tr>
+            </table>
+        </center>
+    </body>
+</html>
